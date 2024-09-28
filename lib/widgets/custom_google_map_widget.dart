d76fa97e7a4b9e4afe8356c24479426f5a104954
ah0es <ahmed099essam@gmail.com>
2024-09-28 20:13:07 +0300

    adding location package

--- lib/widgets/custom_google_map_widget.dart
+++ lib/widgets/custom_google_map_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 import 'package:map_example/models/places_model.dart';
 
 class CustomGoogleMapWidget extends StatefulWidget {
@@ -13,10 +14,13 @@ class _CustomGoogleMapWidgetState extends State<CustomGoogleMapWidget> {
   late GoogleMapController googleMapController;
   String? _mapStyle;
   final Set<Marker> _markers = {};
-
+  final Set<Polyline> _polyLines = {};
+  late Location location;
   @override
   void initState() {
     initMapStyle();
+    location = Location();
+    initLocationServices();
     super.initState();
   }
 
@@ -33,6 +37,16 @@ class _CustomGoogleMapWidgetState extends State<CustomGoogleMapWidget> {
     });
   }
 
+  initLocationServices() async {
+    var isServiceEnabled = await location.serviceEnabled();
+    if (!isServiceEnabled) {
+      var isRequestService = await location.requestService();
+      if (isRequestService) {
+        print('ss');
+      }
+    }
+  }
+
   void initMapMarkers() {
     var myMarkers = places
         .map(
@@ -45,17 +59,32 @@ class _CustomGoogleMapWidgetState extends State<CustomGoogleMapWidget> {
     _markers.addAll(myMarkers);
   }
 
+  void initPolyLines() {
+    const myPolyLines = Polyline(startCap: Cap.roundCap, color: Colors.red, polylineId: PolylineId('1'), points: [
+      LatLng(27.243644005046896, 31.17669837899221),
+      LatLng(27.221916394928794, 31.13309209071705),
+      LatLng(27.266672616962804, 31.149413763848838),
+      LatLng(27.2464447097953, 31.102592099353522)
+    ]);
+
+    setState(() {
+      _polyLines.add(myPolyLines);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         GoogleMap(
+          polylines: _polyLines,
           style: _mapStyle,
           markers: _markers,
           onMapCreated: (controller) {
             googleMapController = controller;
             //  googleMapController.setMapStyle(_mapStyle);
             initMapMarkers();
+            initPolyLines();
           },
           initialCameraPosition: const CameraPosition(
             zoom: 14,
